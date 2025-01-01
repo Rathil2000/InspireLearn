f4d7f47fe7e942dd3a236625d70fa515a295517f
Rathil Thumar <69952347+Rathil2000@users.noreply.github.com>
2025-01-01 16:37:34 +0530

    index.js error handling

--- backend/index.js
+++ backend/index.js
@@ -60,7 +60,10 @@ app.use(contactRoutes);
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("Connected to MongoDB"))
-  .catch((err) => console.error("Error connecting to MongoDB:", err));
+  .catch((err) => {
+    console.error("Error connecting to MongoDB:", err);
+    process.exit(1); // Exit the app if the database connection fails
+  });
 
 // Global Error Handling Middleware
 app.use((err, req, res, next) => {
